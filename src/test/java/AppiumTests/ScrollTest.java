9efd2b5496f8e17bfd47ed7f66e3574a5de385e6
hp <yes2automation@gmail.com>
2020-01-01 08:31:38 +0530

    Swipe and Scroll Updated

--- src/test/java/AppiumTests/ScrollTest.java
+++ src/test/java/AppiumTests/ScrollTest.java
@@ -1,9 +1,34 @@
 package AppiumTests;
 
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.testng.annotations.Test;
 
-public class ScrollTest {
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidTouchAction;
+
+public class ScrollTest extends Base {
+	
   @Test
-  public void f() {
+  public void testScroll() throws MalformedURLException {
+	  AndroidDriver<AndroidElement> driver = startApp();
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  
+	  //test for scrolling the application
+	  driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+	  
+	  //scrolling via AndroidUI Automator, only downward scrolling by this method
+	  System.out.println("Scrolling to Tabs");
+	  driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Tabs\"));");
+	  System.out.println("Scrolled to Tabs");
+	  
+	  //we can scroll via TouchActions however we need to define element after 
+	  //each scroll 
+	  
+	  
   }
 }
--- src/test/java/AppiumTests/SwipeTest.java
+++ src/test/java/AppiumTests/SwipeTest.java
@@ -1,9 +1,40 @@
 package AppiumTests;
 
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class SwipeTest {
+import io.appium.java_client.TouchAction;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static java.time.Duration.ofSeconds;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class SwipeTest extends Base{
+	
   @Test
-  public void f() {
+  public void swipeTest() throws MalformedURLException {
+	  AndroidDriver<AndroidElement> driver = startApp();
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  
+	  //test for swiping the application
+	  driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+	  driver.findElementByAndroidUIAutomator("text(\"Date Widgets\")").click();
+	  driver.findElementByAndroidUIAutomator("text(\"2. Inline\")").click();
+	  driver.findElementByXPath("//*[@content-desc='3']").click();
+	  WebElement swipefrom = driver.findElementByXPath("//*[@content-desc='15']");
+	  WebElement swipeTo = driver.findElementByXPath("//*[@content-desc='45']");
+	  WebElement secondswipeTo = driver.findElementByXPath("//*[@content-desc='10']");
+	  //using Touch Action for swipe here
+	  TouchAction touchAct = new TouchAction(driver);
+	  System.out.println("Swiping from 15 minutes to 45 minutes");
+	  //this swipe is with wait Duration
+	  touchAct.longPress(longPressOptions().withElement(element(swipefrom)).withDuration(ofSeconds(2))).moveTo(element(swipeTo)).release().perform();
+	  System.out.println("Swiping from 45 minutes to 10 minutes");		  
+	//this swipe is without any wait Duration
+	  touchAct.longPress((element(swipeTo))).moveTo(element(secondswipeTo)).release().perform();
   }
 }
